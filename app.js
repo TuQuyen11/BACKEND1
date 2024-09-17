7e90fea59fe000934eb3ee2c36c2f9e1924eaf5d
Tu Quyen <quyenb2113317@student.ctu.edu.vn>
2024-09-17 16:19:10 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -12,19 +12,18 @@ app.use((req, res, next) => {
 	return next(new ApiError(404, "Resource not found"));	
 });
 app.use((err, req, res, next) => {
-	return res.status(error.statusCode || 500).json({
-		message: error.message || "Internal Server Error",
+	return res.status(err.statusCode || 500).json({
+		message: err.message || "Internal Server Error",
 	});
 });
+//6
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
-
-
- 
-
-
-
+app.listen(3000, function(){
+	console.log('Running!');
+})
 module.exports = app;
+
